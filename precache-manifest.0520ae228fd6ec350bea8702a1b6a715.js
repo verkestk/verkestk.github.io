b6e88c80fe6ae8377d8015ce99e3baa52c5deec9
Karlie Verkest <karlie@blacklocus.com>
2019-01-11 21:27:54 -0600

    Updates

---
+++ precache-manifest.0520ae228fd6ec350bea8702a1b6a715.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "19efb12b527d19f9e381",
+    "url": "/static/js/main.19efb12b.chunk.js"
+  },
+  {
+    "revision": "56813b66052e1df5c85a",
+    "url": "/static/js/1.56813b66.chunk.js"
+  },
+  {
+    "revision": "ff4fca9166f12f04e2ae57a74f8e8344",
+    "url": "/index.html"
+  }
+];
