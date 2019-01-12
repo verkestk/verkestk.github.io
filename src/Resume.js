13178b3deebee2062fee3e57e16711f6d12a691e
Karlie Verkest <karlie@blacklocus.com>
2019-01-11 22:34:29 -0600

    cleaning up those charts

--- src/Resume.js
+++ src/Resume.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import FaceIcon from '@material-ui/icons/Face';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import FaceIcon from '@material-ui/icons/Face';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { Cell, Legend, Pie, PieChart, Sector } from 'recharts';
+import { Cell, Pie, PieChart } from 'recharts';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -26,6 +25,9 @@ const styles = theme => ({
     maxWidth: 800,
     margin: '0 auto',
     padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroName: {
+    paddingTop: 20,
   },
   heroButtons: {
     marginTop: theme.spacing.unit * 4,
@@ -46,8 +48,7 @@ const styles = theme => ({
   },
 });
 
-const dataPVP = [{name: 'People', value: 60}, {name: 'Process', value: 40}];
-
+const dataPVP = [{name: 'People', value: 65}, {name: 'Process', value: 35}];
 const dataCVE = [{name: 'Control', value: 10}, {name: 'Enable', value: 90}];
 
 const COLORS_PVP = ['#0088FE', '#00C49F'];
@@ -56,23 +57,45 @@ const COLORS_CVE = ['#FFBB28', '#FF8042'];
 const RADIAN = Math.PI / 180;
 
 const renderCustomizedLabelPVP = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const angle = index == 0 ? 170 : 10;
+  const angle = index === 0 ? 170 : 10;
   const x  = cx + (outerRadius + 10) * Math.cos(-angle * RADIAN);
   const y = cy + (outerRadius + 10) * Math.sin(-angle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">{dataPVP[index].name}
+    <text x={x} y={y}
+          fill="black"
+          fillOpacity={0.54}
+          textAnchor={x > cx ? 'start' : 'end'}
+          dominantBaseline="central"
+          style={{
+                  fontSize: '0.75rem',
+                  fontWeight: 400,
+                  fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                  lineHeight: '1.375em'
+                }}>
+      {dataPVP[index].name}
     </text>
   );
 };
 
 const renderCustomizedLabelCVE = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const angle = index == 0 ? 170 : 10;
+  const angle = index === 0 ? 190 : 350;
   const x  = cx + (outerRadius + 10) * Math.cos(-angle * RADIAN);
   const y = cy + (outerRadius + 10) * Math.sin(-angle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">{dataCVE[index].name}
+    <text x={x} y={y}
+          fill="black"
+          fillOpacity={0.54}
+          textAnchor={x > cx ? 'start' : 'end'}
+          dominantBaseline="central"
+          style={{
+                  fontSize: '0.75rem',
+                  fontWeight: 400,
+                  fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                  lineHeight: '1.375em'
+                }}>
+      {dataCVE[index].name}
     </text>
   );
 };
@@ -96,57 +119,54 @@ function Resume(props) {
         <div className={classes.heroUnit}>
           <div className={classes.heroContent}>
             <Grid container spacing={24}>
-              <Grid item xs={12} sm={6}>
-                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              <Grid item xs={12} sm={7}>
+                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom className={classes.heroName}>
                   Karlie Verkest
                 </Typography>
                 <Typography variant="h6" align="center" color="textSecondary" paragraph>
                   human-focused engineering manager
                 </Typography>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <div>
-                  <PieChart width={300} height={100}>
-                    <Pie
-                      data={dataPVP}
-                      cx={150}
-                      cy={100}
-                      startAngle={180}
-                      endAngle={0}
-                      innerRadius={60}
-                      outerRadius={80}
-                      fill="#8884d8"
-                      label={renderCustomizedLabelPVP}
-                      labelLine={false}
-                      paddingAngle={5}
-                    >
-                    	{
-                      	dataPVP.map((entry, index) => <Cell fill={COLORS_PVP[index % COLORS_PVP.length]}/>)
-                      }
-                    </Pie>
-                  </PieChart>
-                </div>
-                <div>
-                  <PieChart width={300} height={100}>
-                    <Pie
-                      data={dataCVE}
-                      cx={150}
-                      cy={100}
-                      startAngle={180}
-                      endAngle={0}
-                      innerRadius={60}
-                      outerRadius={80}
-                      fill="#8884d8"
-                      label={renderCustomizedLabelCVE}
-                      labelLine={false}
-                      paddingAngle={5}
-                    >
-                    	{
-                      	dataCVE.map((entry, index) => <Cell fill={COLORS_CVE[index % COLORS_CVE.length]}/>)
-                      }
-                    </Pie>
-                  </PieChart>
-                </div>
+              <Grid item xs={12} sm={5}>
+                <PieChart width={260} height={90}>
+                  <Pie
+                    data={dataPVP}
+                    cx={122}
+                    cy={80}
+                    startAngle={180}
+                    endAngle={0}
+                    innerRadius={40}
+                    outerRadius={60}
+                    fill="#8884d8"
+                    label={renderCustomizedLabelPVP}
+                    labelLine={false}
+                    paddingAngle={5}
+                  >
+                  	{
+                    	dataPVP.map((entry, index) => <Cell fill={COLORS_PVP[index % COLORS_PVP.length]}/>)
+                    }
+                  </Pie>
+                </PieChart>
+                <Divider/>
+                <PieChart width={260} height={90}>
+                  <Pie
+                    data={dataCVE}
+                    cx={122}
+                    cy={0}
+                    startAngle={-180}
+                    endAngle={0}
+                    innerRadius={40}
+                    outerRadius={60}
+                    fill="#8884d8"
+                    label={renderCustomizedLabelCVE}
+                    labelLine={false}
+                    paddingAngle={5}
+                  >
+                  	{
+                    	dataCVE.map((entry, index) => <Cell fill={COLORS_CVE[index % COLORS_CVE.length]}/>)
+                    }
+                  </Pie>
+                </PieChart>
               </Grid>
             </Grid>
             <div className={classes.heroButtons}>
